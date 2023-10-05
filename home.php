060829d54678636cb8337e342c983061ce90f394
Jauhara Juli <arajauhara37@gmail.com>
2023-10-05 14:39:02 +0700

    Belajar git app nilai rgi

---
+++ home.php
@@ -0,0 +1,14 @@
+<div>
+    <h2><center>Kelas TKJ RGI Angkatan XXIX</center></h2>
+    <img src="gambar/kuning.png" alt="tkj" width="20%" class="rounded mx-auto d-block"
+    <p>Kelas TKJ merupakan kelasnya para programer handal,  di kelas ini kamu akan diajarkan skill seputar dunia Komputer dan Jaringan. 
+        <br>Materi Kelas TKJ:</p>
+    <ul>
+        <li>mikrotik</li>
+        <li>server</li>
+        <li>web development</li>
+        <li>hardware software</li>
+        <li>menulis kreatif</li>
+    </ul>
+    <p>Lulusan teknik komputer dan jaringan menghasilkan programmer handal yang nantinya akan bergelut didunia IT. <br>Alumni lulusan TKJ banyak yang bekerja di perusahaan IT dan beberapa lembaga yang membutuhkan keahlian mengenai IT.</p>
+</div>
